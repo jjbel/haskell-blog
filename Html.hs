5fb049167b8ba8b975f6ea864c884d1d99e310c5
jjbel <jaibellare@gmail.com>
2024-09-07 23:50:49 +0530

    add Html module

---
+++ Html.hs
@@ -0,0 +1,49 @@
+module Html
+  ( Html
+  , Title
+  , Structure
+  , html_
+  , p_
+  , h1_
+  , append_
+  , render
+  )
+  where
+
+newtype Html
+  = Html String
+
+newtype Structure
+  = Structure String
+
+type Title
+  = String
+
+html_ :: Title -> Structure -> Html
+html_ title content =
+  Html
+    ( el "html"
+      ( el "head" (el "title" title)
+        <> el "body" (getStructureString content)
+      )
+    )
+
+p_ :: String -> Structure
+p_ = Structure . el "p"
+
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
+
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+append_ :: Structure -> Structure -> Structure
+append_ c1 c2 =
+  Structure (getStructureString c1 <> getStructureString c2)
+
+getStructureString :: Structure -> String
+getStructureString (Structure str) = str
+
+render :: Html -> String
+render (Html str) = str
--- hello.hs
+++ hello.hs
@@ -1,3 +1,5 @@
+import Html
+
 main :: IO ()
 main = putStrLn (render myhtml)
 
@@ -12,41 +14,3 @@ myhtml =
         (p_ "Paragraph #2")
       )
     )
-
-newtype Html
-  = Html String
-
-newtype Structure
-  = Structure String
-
-type Title
-  = String
-
-html_ :: Title -> Structure -> Html
-html_ title content =
-  Html
-    ( el "html"
-      ( el "head" (el "title" title)
-        <> el "body" (getStructureString content)
-      )
-    )
-
-p_ :: String -> Structure
-p_ = Structure . el "p"
-
-h1_ :: String -> Structure
-h1_ = Structure . el "h1"
-
-el :: String -> String -> String
-el tag content =
-  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
-
-append_ :: Structure -> Structure -> Structure
-append_ c1 c2 =
-  Structure (getStructureString c1 <> getStructureString c2)
-
-getStructureString :: Structure -> String
-getStructureString (Structure str) = str
-
-render :: Html -> String
-render (Html str) = str
