0708d407b1f8a53d77271a36d2123ebccb4fe7cd
jjbel <jaibellare@gmail.com>
2024-09-07 23:47:02 +0530

    add Html, Structure types

--- hello.hs
+++ hello.hs
@@ -1,1 +1,52 @@
-main = putStrLn "<html><body>Hello World!</body></html>"
+main :: IO ()
+main = putStrLn (render myhtml)
+
+myhtml :: Html
+myhtml =
+  html_
+    "My title"
+    ( append_
+      (h1_ "Heading")
+      ( append_
+        (p_ "Paragraph #1")
+        (p_ "Paragraph #2")
+      )
+    )
+
+newtype Html
+  = Html String
+
+newtype Structure
+  = Structure String
+
+type Title
+  = String
+
+html_ :: Title -> Structure -> Html
+html_ title content =
+  Html
+    ( el "html"
+      ( el "head" (el "title" title)
+        <> el "body" (getStructureString content)
+      )
+    )
+
+p_ :: String -> Structure
+p_ = Structure . el "p"
+
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
+
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+append_ :: Structure -> Structure -> Structure
+append_ c1 c2 =
+  Structure (getStructureString c1 <> getStructureString c2)
+
+getStructureString :: Structure -> String
+getStructureString (Structure str) = str
+
+render :: Html -> String
+render (Html str) = str
